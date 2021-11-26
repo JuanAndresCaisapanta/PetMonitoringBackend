66837db3d918a2897de5c0e5e320ce3b19bd836c
Juan Caisapanta <juancaisapanta@hotmail.com>
2021-11-26 03:02:20 -0500

    Inicio Proyecto

---
+++ src/main/java/com/MonitoreoMascotas/MonitoreoMascotasApplication.java
@@ -0,0 +1,13 @@
+package com.MonitoreoMascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoMascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreoMascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MonitoreoMascotas/MonitoreoMascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MonitoreoMascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoMascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
