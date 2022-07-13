8556d798ca345f4098e43e3f5f701fb793fcac67
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2022-07-13 00:43:56 -0500

    cors in main

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM ibmjava:jre
-RUN mkdir /opt/app
-COPY target/MonitoreoMascotas-0.0.1-SNAPSHOT.jar /opt/app
-CMD ["java", "-jar", "/opt/app/MonitoreoMascotas-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/Pet_Monitoring/Pet_MonitoringApplication.java
+++ src/main/java/com/Pet_Monitoring/Pet_MonitoringApplication.java
@@ -1,13 +1,36 @@
 package com.Pet_Monitoring;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Pet_MonitoringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pet_MonitoringApplication.class, args);
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://moniopet.vercel.app"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 	
 }
